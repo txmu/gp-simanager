07a9f8444a7f7e33feef33edd1af83b10b30deb2
txmu <71213934+txmu@users.noreply.github.com>
2026-02-09 20:30:33 +0800

    add A-ADS

--- public/sw.js
+++ public/sw.js
@@ -1,9 +1,9 @@
 /**
  * 全球通套餐管家 - 手动 Service Worker
- * VERSION: v4.4
+ * VERSION: v4.5
  */
 
-const VERSION = 'v4.4';
+const VERSION = 'v4.5';
 const CACHE_NAME = `sim-manager-cache-${VERSION}`;
 
 // 核心静态资源缓存列表
