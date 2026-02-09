782ac17da1e251ee1decab164966c414507e23b1
txmu <71213934+txmu@users.noreply.github.com>
2026-02-09 19:00:09 +0800

    small changes

--- public/sw.js
+++ public/sw.js
@@ -1,9 +1,9 @@
 /**
  * 全球通套餐管家 - 手动 Service Worker
- * VERSION: v4.1
+ * VERSION: v4.3
  */
 
-const VERSION = 'v4.2';
+const VERSION = 'v4.3';
 const CACHE_NAME = `sim-manager-cache-${VERSION}`;
 
 // 核心静态资源缓存列表
